a7ac64f049f7fd39b7659dd6853c9a9e9f893912
PedrinhowVieira <pedro.dmvieira@gmail.com>
2021-02-25 18:34:57 -0300

    remove husky config from package.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
 
-const foo = 'bla'
-
 export default function Home({ title = 'React' }) {
   return (
     <div className={styles.container}>
